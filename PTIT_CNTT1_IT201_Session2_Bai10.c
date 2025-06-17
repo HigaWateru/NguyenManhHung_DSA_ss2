09cd60725f393bfd1ce09135d98ba5e9c0468985
Nguyễn Mạnh Hùng <106744156+HigaWateru@users.noreply.github.com>
2025-06-17 08:53:50 +0700

    Update PTIT_CNTT1_IT201_Session2_Bai10.c

--- PTIT_CNTT1_IT201_Session2_Bai10.c
+++ PTIT_CNTT1_IT201_Session2_Bai10.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
 #include <stdlib.h>
 
-int a[10000], n, count[10000], min, max, maxCount;
+int *a, n, count[10000], min, max, maxCount;
 int main() {
     scanf("%d", &n);
+    a = (int *)malloc(n * sizeof(int));
     for(int i = 0; i < n; i++) {
         scanf("%d", &a[i]);
         count[a[i]]++;
@@ -11,6 +12,7 @@ int main() {
         max = a[i] > max ? a[i] : max;
         maxCount = count[a[i]] > maxCount ? count[a[i]] : maxCount;
     }
+    free(a);
     for(int i = min; i <= max; i++) if (count[i] > 0) printf("Phan tu %d xuat hien %d lan\n", i, count[i]);
     return 0;
 }
